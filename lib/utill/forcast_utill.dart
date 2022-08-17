2a4fbd0c5437e264daa2284a8dd9361ea59ae708
NATNAEL BIRHANU <negusnati@gmail.com>
2022-08-17 06:07:06 -0700

    forcast utill

---
+++ lib/utill/forcast_utill.dart
@@ -0,0 +1,7 @@
+
+
+String gg = "https://api.openweathermap.org/data/2.5/forecast/daily?q=London&cnt=7&appid=ed60fcfbd110ee65c7150605ea8aceea&units=metric";
+
+class Util{
+  static String appId = "ed60fcfbd110ee65c7150605ea8aceea";
+}
