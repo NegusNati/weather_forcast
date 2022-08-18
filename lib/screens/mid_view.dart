f5f67d5fdd23a30066366af49e1170575d9b4a64
NATNAEL BIRHANU <negusnati@gmail.com>
2022-08-18 06:35:02 -0700

    FontAwesomeIcons package

--- lib/screens/mid_view.dart
+++ lib/screens/mid_view.dart
@@ -1,6 +1,9 @@
 
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:weather_forcast/utill/forcast_utill.dart';
+
+import '../utill/constants.dart';
 
 Widget midview(AsyncSnapshot snapshot){
   var forecastList = snapshot.data.list; 
@@ -18,7 +21,7 @@ Widget midview(AsyncSnapshot snapshot){
           style: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),),
           Text(formatedDate, style:const TextStyle(fontSize: 15) ),
           const SizedBox(height: 15,),
-          const Icon(Icons.sunny, size: 190,),
+            Icon(FontAwesomeIcons.cloud, color: Colors.pink[300]  , size: 190,),
           Padding(
             padding: const EdgeInsets.all(12.0),
             child: Column(
@@ -29,14 +32,45 @@ Widget midview(AsyncSnapshot snapshot){
                   mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Text("${forecastList[0].temp.day.toStringAsFixed(0)}°C", style: TextStyle(fontSize: 35, fontWeight: FontWeight.w600) ),
+                    Text("${forecastList[0].temp.day.toStringAsFixed(0)}°C", style: const TextStyle(fontSize: 35, fontWeight: FontWeight.w600) ),
                     
                     const SizedBox(width: 10,),
-                    Text("${forecastList[0].weather[0].description.toUpperCase()}", style: TextStyle(fontSize: 25, fontWeight: FontWeight.w200) ),
+                    Text("${forecastList[0].weather[0].description.toUpperCase()}", style: const TextStyle(fontSize: 25, fontWeight: FontWeight.w200) ),
                   
                   ],
                 ),
-                Row(),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12,vertical: 12),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                      
+                        children: [
+                          Text("${forecastList[0].speed.toStringAsFixed(0)} M/s ",style:kSmallTextStyle),
+                          const SizedBox(height: 5 ),
+                          const Icon(FontAwesomeIcons.wind , size: 20, color:Colors.brown )
+                        ],
+                      ),
+                      Column(
+                        children:  [
+                          Text("${forecastList[0].humidity}% ",style:kSmallTextStyle),
+                          const SizedBox(height: 5 ),
+
+                          const Icon(FontAwesomeIcons.faceGrinBeamSweat , size: 20, color:Colors.brown )
+                        ],
+                      ),
+                      Column(
+                        children:  [
+                          Text("${forecastList[0].temp.day.toStringAsFixed(0)}°C ", style:kSmallTextStyle ),
+                          const SizedBox(height: 5 ),
+                           Icon(FontAwesomeIcons.temperatureHalf, color: Colors.brown[400]),
+                          // const Icon(Icons.telegram_sharp , size: 20, color:Colors.brown )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           )
---
+++ lib/utill/constants.dart
@@ -0,0 +1,9 @@
+
+
+import 'package:flutter/cupertino.dart';
+
+const kSmallTextStyle = TextStyle(
+  fontSize: 18, 
+  fontWeight: FontWeight.w300,
+  wordSpacing: 2,
+);
