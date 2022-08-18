9b7127430ef928e9dc31a526ca060884cd38927a
NATNAEL BIRHANU <negusnati@gmail.com>
2022-08-18 06:00:23 -0700

    temprature and description

--- lib/screens/mid_view.dart
+++ lib/screens/mid_view.dart
@@ -1,5 +1,5 @@
 
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:weather_forcast/utill/forcast_utill.dart';
 
 Widget midview(AsyncSnapshot snapshot){
@@ -16,7 +16,30 @@ Widget midview(AsyncSnapshot snapshot){
         children: [
           Text("$cityNameUi, $cityCountry",
           style: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),),
-          Text(formatedDate),
+          Text(formatedDate, style:const TextStyle(fontSize: 15) ),
+          const SizedBox(height: 15,),
+          const Icon(Icons.sunny, size: 190,),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text("${forecastList[0].temp.day.toStringAsFixed(0)}°C", style: TextStyle(fontSize: 35, fontWeight: FontWeight.w600) ),
+                    
+                    const SizedBox(width: 10,),
+                    Text("${forecastList[0].weather[0].description.toUpperCase()}", style: TextStyle(fontSize: 25, fontWeight: FontWeight.w200) ),
+                  
+                  ],
+                ),
+                Row(),
+              ],
+            ),
+          )
         ],),
     )
   );
