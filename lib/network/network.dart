466f5df81e311aaf186dbd04e0284e1a54ce0fae
NATNAEL BIRHANU <negusnati@gmail.com>
2022-08-22 03:13:56 -0700

    toast package

--- lib/network/network.dart
+++ lib/network/network.dart
@@ -1,31 +1,50 @@
 import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:toast/toast.dart';
 import 'package:weather_forcast/utill/forcast_utill.dart';
 import 'package:http/http.dart';
 
 import '../models/weather_model.dart';
 
-class Network{
-
-  Future<WeatherForecastModel> getWeatherForcast({required String cityName}) async {
-
-     var url =  "https://api.openweathermap.org/data/2.5/forecast/daily?q="+cityName+"&cnt=7&appid="+Util.appId+"&units=metric";
-
-     final Response response = await get(Uri.parse(url));
-     print(Uri.parse(url));
+class Network {
+ 
 
 
-    
+  Future<WeatherForecastModel> getWeatherForcast(
+      {required String cityName}) async {
+    var url = "https://api.openweathermap.org/data/2.5/forecast/daily?q=$cityName&cnt=7&appid=${Util.appId}&units=metric";
 
-     if(response.statusCode == 200){
-      print(json.decode(response.body));
-      //return the mapped json to our PODO model  
-      return WeatherForecastModel.fromJson(json.decode(response.body)); 
-     }else{
-      throw Exception("Error reaching the wearther API");
-     }
+    final Response response = await get(Uri.parse(url));
+    print(Uri.parse(url));
 
-  }
-
+    // try {
+      
+    //     print(json.decode(response.body));
+    //     //return the mapped json to our PODO model
+    //     return WeatherForecastModel.fromJson(json.decode(response.body));
+      
+    // } catch (e) {
+    //   print('not found');
+    //   throw  Exception("Error reaching the wearther API");
+    // }
+          
   
+
+    if (response.statusCode == 200) {
+      print(json.decode(response.body));
+      //return the mapped json to our PODO model
+      return WeatherForecastModel.fromJson(json.decode(response.body));
+    }else if (response.statusCode == 404){
+      // ignore: use_build_context_synchronously
+    
+      Toast.show("Toast plugin app", duration: Toast.lengthShort, gravity:  Toast.bottom);
+      throw SizedBox(width: 1,);
+    } 
+    else {
+      print(response.statusCode );
+      throw Exception("Error reaching the wearther API");
+    }
+  }
 }
